e9bbf5146dda0c2bd669f832e49fa2f542ce444a
Zy5879 <ZaireM21@gmail.com>
2022-09-23 02:01:35 -0400

    X and O alternates after you click first grid

--- script.js
+++ script.js
@@ -1,46 +1,29 @@
 const gameBoard = (function() {
-    let gameArray = ['X']
+    let gameArray = []
     return {
         gameArray,
     }
 })();
 
 const playerFactories = (assignedvariable) => {
-    return(assignedvariable)
 }
 
-let displayController = (function() {
+let game = (function() {
+    let player_one = 1
     const btns = document.querySelectorAll('.grid-item')
-    btns.forEach(btn => {
+   btns.forEach(btn => {
         btn.addEventListener('click', function() {
-            btn.innerHTML = gameBoard.gameArray;
+            btn.disabled = true;
+            btn.style.color = 'black'
+            if(player_one == 1) {
+                btn.innerHTML = 'X';
+                player_one = 0;
+        
+            } else {
+                btn.innerHTML = 'O'
+                player_one = 1;
+            }
+            gameBoard.gameArray.push(btn.innerHTML);
         })
     })
-    // btns.forEach(function(currentBtn) {
-    //     currentBtn.addEventListener('click', function() {
-            
-    //     })
-    // })
-// const cells = document.querySelectorAll('.grid-item');
-// cells.forEach(cell => {
-//     cell.addEventListener('click',handleClick)
-// })
-
-// function handleClick() {
-//     if(cells)
-// }
-// for(i = 0; i<cells.length; i++) {
-//     cells[i].addEventListener('click', (e) => {
-        
-//     });
-// }
-// for(let cell of cells) {
-//     cell.addEventListener('click',handleClick)
-// }
-    // cells.forEach(cell => {
-    //     cell.addEventListener('click',)
-    //     })
-// cells[0].innerHTML = gameBoard.gameArray;
-// console.log(gameBoard.gameArray)
-// return{};
 })();
