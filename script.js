990232d4634dc1a8bb0481c5281688e94ec808e7
Zy5879 <ZaireM21@gmail.com>
2022-09-28 20:28:41 -0400

    added function to check winner

--- script.js
+++ script.js
@@ -40,32 +40,46 @@ const displayController = (function() {
                 }
                 gameBoard.gameArray.push(btn.innerHTML);
                 checkWinner()
-            })   
+              
+            })  
       })
       function checkWinner() {
+    //   if(btn[0] = 'X') {
+    //     wins.innerHTML = 'Tie'
+    //   }
         // let win = true;
         // if(win) {
             for(let solution of winningSolutions) {
                 if(solution.every(i => btns[i].innerHTML == 'X')) {
                     wins.innerHTML = 'X WON';
-                    // win = false;
+                    
+                //     // win = false;
                 } else if(solution.every(i => btns[i].innerHTML == 'O')) {
                     wins.innerHTML = 'O WON'
-                    // win = false;
-                }else {
-                    wins.innerHTML = 'Tie'
+                } else if(solution.every(i => btns[i].innerHTML && gameBoard.gameArray.length == 9)) {
+                    wins.innerHTML = 'TIE'
                 }
+
+
+                    // win = false;
+                // } else if(solution.every(i => btns[i].innerHTML == 'X' && 'O' )) {
+                //     wins.innerHTML = 'Tie';
+                // }
+            
                     // win = true;
                 // }
                 // } else if(solution.every(i => btns[i].innerHTML != 'X' && 'O')) {
                 //     wins.innerHTML = 'Tie';
                 //     win = false;
                 // }
-            
+            // }
+            }
         }
-
-    }
-
+    // function checkTie() {
+    //     if(gameBoard.gameArray.length == 9) {
+    //         wins.innerHTML = 'Tie'
+    //     }
+    // }
 })();
 
 
@@ -85,3 +99,25 @@ const displayController = (function() {
 //     }
 // })();
 //TODO LIST CREATE A WINNING SOLUTON
+
+// const winningSolutions = [
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,8],
+
+//     [0,3,6],
+//     [1,4,7],
+//     [2,5,8],
+
+//     [0,4,8],
+//     [2,4,6],
+// ]
+
+// for(let i = 0; i < winningSolutions.length; i++) {
+//     let combos = winningSolutions[i];
+
+//     // for(let k = 0; k < combos.length; k++) {
+//     //    let winCombo = combos[k]
+//     //    console.log(winCombo);
+//     // }
+// }
