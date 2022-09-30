152c6092c8d5be84f1ad3cca7ca8fc4865dfb4cd
Zy5879 <ZaireM21@gmail.com>
2022-09-30 12:21:57 -0400

    clened up code

--- script.js
+++ script.js
@@ -1,109 +1,6 @@
-const winner = document.querySelectorAll('.win p')
-
-
-const btns = document.querySelectorAll('.grid-item');
-const wins = document.querySelector('.wins')
-const turn = document.getElementById('turn')
-const winningSolutions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-
-    [0,4,8],
-    [2,4,6],
-]
-
-
-const gameBoard = (function() {
-    let gameArray = []
-    return {
-        gameArray,
-    }
-})();
-
-const playerFactories = (name) => {
-    return {name};
-}
-
-const displayController = (function() {
-    let counter = 1
-    const playerOne = playerFactories('Player 1')
-    const playerTwo = playerFactories('Player 2')
-    const grid = document.querySelector('.game-grid')
-    const start = document.getElementById('start')
-    const reset = document.getElementById('reset');
-    start.addEventListener('click', startGame)
-       
-    function startGame() {
-            grid.style.display = '';
-            start.style.display = 'none';
-        }
-
-        reset.addEventListener('click', restart)
-            function restart () {
-                window.location.reload()
-            }
-
-
-
-        btns.forEach(btn => {
-            btn.addEventListener('click', handleClick)
-            
-                function handleClick () {
-                btn.disabled = true;
-                btn.style.color = 'black';
-                if(counter == 1) {
-                    btn.innerHTML = 'X'
-                    counter = 0;
-                    turn.innerHTML = `${playerTwo.name}, Your Move!`
-                } else {
-                    btn.innerHTML = 'O'
-                    counter = 1;
-                    turn.innerHTML = `${playerOne.name}, Your Move!`
-                }
-                gameBoard.gameArray.push(btn.innerHTML);
-                checkWinner()
-            
-        }
-    })
-    
-
-      function checkWinner() {
-            for(let solution of winningSolutions) {
-                if(solution.every(i => btns[i].innerHTML == 'X')) {
-                    turn.innerHTML = `Congratulations ${playerOne.name}`;
-                    
-                } else if(solution.every(i => btns[i].innerHTML == 'O')) {
-                    turn.innerHTML = `Congratulations ${playerTwo.name}`;
-                } else if(solution.every(i => btns[i].innerHTML && gameBoard.gameArray.length == 9)) {
-                    turn.innerHTML = 'Its A Tie'
-                }
-            }
-        }
-})();
-
-
-
-// const game = (function () {
-//     let win = true;
-//     if(win) {
-//         for(let solution of winningSolutions) {
-//             if(solution.every(i => btns[i] = 'X')) {
-//                 wins.innerHTML = 'X WON';
-//                 win = false
-//             } else if (solution.every(i => displayController[i] = 'O')) {
-//                 wins.innerHTML = 'O WON'
-//                 win = false
-//             }
-//         }
-//     }
-// })();
-//TODO LIST CREATE A WINNING SOLUTON
-
+// const btns = document.querySelectorAll('.grid-item');
+// const wins = document.querySelector('.wins')
+// const turn = document.getElementById('turn')
 // const winningSolutions = [
 //     [0,1,2],
 //     [3,4,5],
@@ -117,11 +14,121 @@ const displayController = (function() {
 //     [2,4,6],
 // ]
 
-// for(let i = 0; i < winningSolutions.length; i++) {
-//     let combos = winningSolutions[i];
 
-//     // for(let k = 0; k < combos.length; k++) {
-//     //    let winCombo = combos[k]
-//     //    console.log(winCombo);
-//     // }
-// }
+const gameBoard = (function() {
+    let gameArray = []
+    // const winningSolutions = [
+    //     [0,1,2],
+    //     [3,4,5],
+    //     [6,7,8],
+    
+    //     [0,3,6],
+    //     [1,4,7],
+    //     [2,5,8],
+    
+    //     [0,4,8],
+    //     [2,4,6],
+    // ]
+
+    return {
+        gameArray
+    }
+})();
+
+const playerFactories = (name) => {
+    return {name};
+}
+
+const game = (function() {
+
+ //winning solutions
+    const winningSolutions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+    
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+    
+        [0,4,8],
+        [2,4,6],
+    ]
+//winning solutions
+
+
+    let counter = 1
+    const playerOne = playerFactories('Player 1')
+    const playerTwo = playerFactories('Player 2')
+
+    const grid = document.querySelector('.game-grid');
+    const reset = document.getElementById('reset');
+
+    const btns = document.querySelectorAll('.grid-item')
+
+    const turn = document.getElementById('turn')
+
+
+        const start = document.getElementById('start')
+        start.addEventListener('click', startGame)
+        function startGame () {
+            grid.style.display = '';
+            start.style.display = 'none';
+            console.log('Let The Games Begin')
+               } 
+
+
+        reset.addEventListener('click', restart)
+            function restart () {
+                window.location.reload()
+                console.log('Game restarted')
+            }
+
+
+
+        btns.forEach(btn => {
+            btn.addEventListener('click', handleClick)
+
+            function handleClick () {
+                btn.disabled = true;
+                btn.style.color = 'black';
+                if(counter == 1) {
+                    btn.innerHTML = 'X'
+                    counter = 0;
+                    turn.innerHTML = `${playerTwo.name}, Your Move!`
+                    console.log('Players Switched')
+                } else {
+                    btn.innerHTML = 'O'
+                    counter = 1;
+                    turn.innerHTML = `${playerOne.name}, Your Move!`
+                    console.log('Players Switched Again')
+                }
+                gameBoard.gameArray.push(btn.innerHTML);
+               checkWinner()
+            
+        }
+    })
+    
+
+      function checkWinner() {
+            for(let solution of winningSolutions) {
+                if(solution.every(i => btns[i].innerHTML == 'X')) {
+                    turn.innerHTML = `Congratulations ${playerOne.name}`;
+                    console.log('X is Winner')
+                } else if(solution.every(i => btns[i].innerHTML == 'O')) {
+                    turn.innerHTML = `Congratulations ${playerTwo.name}`;
+                    console.log('O is Winner')
+                } else if(solution.every(i => btns[i].innerHTML && gameBoard.gameArray.length == 9)) {
+                    turn.innerHTML = 'Its A Tie'
+                    console.log('Its A tie')
+                }
+            }
+        }
+    return {
+        checkWinner, 
+        restart,
+        startGame
+    }
+})();
+
+
